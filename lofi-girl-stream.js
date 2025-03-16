4a94477ed4198a38c91357515b444d7582d70a0c
Nghis Duc Tran <funnycomics5005@gmail.com>
2025-03-16 11:05:47 +1030

    Updated lofi-girl-stream.js

--- lofi-girl-stream.js
+++ lofi-girl-stream.js
@@ -3,8 +3,7 @@ let playlist = [
     { id: "jfKfPfyJRdk", title: "lofi hip hop radio 📚 beats to relax/study to" },
     { id: "5yx6BWlEVcY", title: "Chillhop Radio - jazzy & lofi hip hop beats 🐾" },
     { id: "pHoADRY_WSM", title: "Espresso Coffee 🍵 Coffee Shop Lofi 🌴 Beats for work / relax [ lofi hip hop ~ lofi cafe ]" },
-    { id: "SyqT7_K164", title: "lofi hip hop mix 🚃 chill beats for studying and relaxing"),
-" },
+    { id: "-SyqT7_K164", title: "lofi hip hop mix 🚃 chill beats for studying and relaxing"},
 ];
 
 let currentSongIndex = 0;
