0aed44926ed19e02999caec07b976213891de223
Nghis Duc Tran <funnycomics5005@gmail.com>
2025-05-11 11:10:24 +0930

    version 2.4.4

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ let playlist = [
     { id: "O8MYZY6sFpI", title: "animal crossing ~ new horizons lofi" },
     { id: "1P5BSm_oFJg", title: "Lofi Girl - Snowman (Music Video)" },
     { id: "gv7hcXCnjOw", title: "(no copyright music) jazz type beat “sunset” | royalty free vlog music | prod. by lukrembo" },
-    { id: "YTUF1o9Sf3E", title:"lukrembo - affogato (royalty free vlog music)"}
-    { id: "EtZ2m2Zm3vY", title:"(no copyright music) lofi type beat “biscuit” | free vlog music | prod. by lukrembo"}
-    
+    { id: "YTUF1o9Sf3E", title:"lukrembo - affogato (royalty free vlog music)"},
+    { id: "EtZ2m2Zm3vY", title:"(no copyright music) lofi type beat “biscuit” | free vlog music | prod. by lukrembo"},
 
 ];
 
