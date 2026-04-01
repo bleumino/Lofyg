a33d319e66355d6fdbfe9244dd43c7be5a370bde
Nghis Duc Tran <funnycomics5005@gmail.com>
2026-04-02 09:44:35 +1030

    Update Stillwaves.js

--- Stillwaves.js
+++ Stillwaves.js
@@ -270,6 +270,8 @@ document.head.appendChild(style);
       {id: "TJZFrDoz0S8", title: "Lilas Ikuta (幾田りら) - Lens 「レンズ」Lyrics Video [Kan/Rom/Eng]", moods:["vibes"], languages: ["japanese"], backgroundType: "lyrics-video"},
       {id: "n53dV2hEhVA", title: "スパークル - From THE FIRST TAKE", moods:["vibes"], languages: ["japanese"], backgroundType: "normal-video"},
       {id:"h_IKqQ9-4OU", title: "VIDEOCLUB - Amour Plastique", moods:["vibes"], languages: ["french"], backgroundType: "normal-video"},
+      {id:"pP6B1CAFkFk", title: "прости меня - Алевтина Дурова", moods:["vibes"], languages: ["russian"], backgroundType: "normal-video"},
+      {id:"xEaqBSk4fJQ?", title: "Намёки - левтина Дурова", moods:["vibes"], languages: ["russian"], backgroundType: "normal-video"},
        
   
      
